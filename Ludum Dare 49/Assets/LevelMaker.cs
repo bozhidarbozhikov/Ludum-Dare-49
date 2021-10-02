35e88908bc4b77516b50fdf17720826fc0895184
bozhidarbozhikov <bozhidarbozhikov_j18@schoolmath.eu>
2021-10-02 10:57:03 +0300

    Level Maker

---
+++ Ludum Dare 49/Assets/LevelMaker.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class LevelMaker : MonoBehaviour
+{
+    public Tilemap tilemap;
+
+    [SerializeField]
+    public Sprite[] tiles;
+
+    public GameObject[] objects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateLevel();
+    }
+
+    void CreateLevel()
+    {
+        foreach (Vector3Int tilePos in tilemap.cellBounds.allPositionsWithin)
+        {
+            Sprite sprite = tilemap.GetSprite(tilePos);
+
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                if (sprite == tiles[i]) PlacePieces(tilePos, i);
+            }
+        }
+    }
+
+    void PlacePieces(Vector3Int tilePos, int index)
+    {
+        Instantiate(objects[index], tilemap.CellToWorld(tilePos) + new Vector3(0.5f, 0, 0.5f), Quaternion.Euler(-90, 0, 0));
+    }
+}
