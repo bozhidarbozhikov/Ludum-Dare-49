11665f7f0230bef85914c42ea15a5ee664361866
bozhidarbozhikov <bozhidarbozhikov_j18@schoolmath.eu>
2021-10-02 16:45:11 +0300

    Rotate in movement

--- Ludum Dare 49/Assets/Scripts/PlayerController.cs
+++ Ludum Dare 49/Assets/Scripts/PlayerController.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float rotateSpeed = 5f;
 
     public Transform movePoint;
 
     public LayerMask whatStopsMovement;
 
-    private Vector3 rotateTo;
-
     // Start is called before the first frame update
     void Start()
     {
         movePoint.parent = null;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-        transform.rotation = Vector3.RotateTowards();
+
+        //transform.eulerAngles = Vector3.RotateTowards(transform.rotation.eulerAngles, rotateTo, 6.28318531f, rotateSpeed * Time.deltaTime);
+        //transform.eulerAngles = rotateTo;
 
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
@@ -32,6 +34,9 @@ public class PlayerController : MonoBehaviour
                 //if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, 0f, Input.GetAxisRaw("Horizontal")), 0.6f, whatStopsMovement))
                 {
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+
+                    StopAllCoroutines();
+                    StartCoroutine(LerpFunction(Quaternion.Euler(GetEulerForRotate(new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f))), rotateSpeed));
                 }
             }
 
@@ -42,10 +47,34 @@ public class PlayerController : MonoBehaviour
                 //if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, 0f, Input.GetAxisRaw("Vertical")), 0.6f, whatStopsMovement))
                 {
                     movePoint.position += new Vector3(0f, 0f, Input.GetAxisRaw("Vertical"));
+
+                    StopAllCoroutines();
+                    StartCoroutine(LerpFunction(Quaternion.Euler(GetEulerForRotate(new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f))), rotateSpeed));
                 }
             }
         }
     }
 
+    IEnumerator LerpFunction(Quaternion endValue, float duration)
+    {
+        float time = 0;
+        Quaternion startValue = transform.rotation;
 
+        while (time < duration)
+        {
+            transform.rotation = Quaternion.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.rotation = endValue;
+    }
+
+    Vector3 GetEulerForRotate(Vector3 direction)
+    {
+        if (direction == Vector3.up) return new Vector3(-90, 0, -180);
+        if (direction == Vector3.down) return new Vector3(-90, 0, 0);
+        if (direction == Vector3.right) return new Vector3(-90, 0, -90);
+        if (direction == Vector3.left) return new Vector3(-90, 0, 90);
+        else return Vector3.zero;
+    }
 }
