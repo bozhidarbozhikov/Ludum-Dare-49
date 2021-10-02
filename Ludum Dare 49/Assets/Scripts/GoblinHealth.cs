4651a07489fdcc23567cb9780072650387140e70
NikolayVelichkov16 <nikivelichkov04@gmail.com>
2021-10-02 17:31:18 +0300

    damage i goblin hp

---
+++ Ludum Dare 49/Assets/Scripts/GoblinHealth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinHealth : MonoBehaviour
+{
+    public int startingHealth;
+    private int currentHealth;
+
+    private void OnEnable()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
